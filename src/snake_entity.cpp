9ddd2a51b94b743e108f5f25bd077a93cf29781c
nik h <niklas.harnish@hotmail.com>
2026-03-10 17:30:44 +0100

    started workign on snake entity

---
+++ src/snake_entity.cpp
@@ -0,0 +1,23 @@
+#include "gameboard_utils.h"
+#include "square.h"
+#include "vao_wrapper.h"
+#include <memory>
+#include "snake_entity.h"
+
+Snake::Snake(GameBoardPos spawnPos, std::shared_ptr<VaoWrapper> vao, std::shared_ptr<Shader> shader) : 
+    squareCount(0), vao(vao), shader(shader), currMovDirection(defaultMovDirection)
+{}
+
+Snake::~Snake() {
+    for (int i = 0; i < squareCount; i++) {
+        delete renderSquares[i];
+    }
+}
+
+
+
+Square* Snake::makeSquare(GameBoardPos pos) {
+    GLPos glPos = GameBoardUtils::translateBoardCoordsToGL(pos);
+    Square* square = new Square(this->vao, this->shader, COLR.getPrepared(), glPos);
+    return square;
+}
---
+++ src/snake_entity.h
@@ -0,0 +1,42 @@
+#ifndef SNAKE_ENTITY_H
+#define SNAKE_ENTITY_H
+
+#include "color.h"
+#include "gameboard_utils.h"
+#include "square.h"
+#include "vao_wrapper.h"
+#include <memory>
+
+enum MovementDirection {
+    Up,
+    Down,
+    Left,
+    Right
+};
+
+class Snake {
+    private:
+        static constexpr int MAX_SQUARES = 512;
+        static constexpr Color COLR = Color(2, 212, 48);
+        static constexpr MovementDirection defaultMovDirection = Right;
+
+    public:
+        GameBoardPos currHeadPos;
+
+        int squareCount;
+        Square* renderSquares[MAX_SQUARES];  
+        GameBoardPos squarePositions[MAX_SQUARES];
+
+        Snake(GameBoardPos spawnPos, std::shared_ptr<VaoWrapper> vao, std::shared_ptr<Shader> shader);
+        ~Snake();
+
+    private:
+        Square* makeSquare(GameBoardPos pos);
+        std::shared_ptr<VaoWrapper> vao;
+        std::shared_ptr<Shader> shader;
+
+        MovementDirection currMovDirection;
+};
+
+
+#endif
